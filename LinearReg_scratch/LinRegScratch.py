a7e3a8dc70209873bed67f6c6266307845dc6d15
lordbecerril <becerrilblas@gmail.com>
2020-03-04 18:34:47 -0800

    Cleaned it up

--- LinearReg_scratch/LinRegScratch.py
+++ LinearReg_scratch/LinRegScratch.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import math
 import random
-#import matplotlib.pyplot as plt
 import statistics
 
 
@@ -12,16 +11,11 @@ MNIST15_15_df = pd.read_csv("MNIST_15_15.csv", header = None)
 MNIST15_15_labels = pd.read_csv("MNIST_LABEL.csv", header = None)
 
 
-def generateImages(data):
-    #print(data.loc[0:0,1:])
-    data[0].to_csv("./images/labels")
-    for i in range(len(data.index)):
-        img = np.array(data.loc[i:i,1:]).reshape(15, 15)
-        #plt.imshow(img)
-        #plt.show()
-        #plt.savefig("./images/"+str(i)+".png")
 
 def SolverLinearRegression(X, y):
+    """
+        Does Magic
+    """
     XTdotX = np.dot(X.transpose(), X)
     #print("np.dot(X.transpose(), X) is:")
     #print(XTdotX)
@@ -30,6 +24,9 @@ def SolverLinearRegression(X, y):
 
 
 def CreateDataFrame(data, Normalized):
+    """
+        Combines the labels and the normalized data into one data frame and returns it
+    """
     # Verify Length of data
     #print(len(data.values))
 
@@ -56,52 +53,48 @@ def CreateDataFrame(data, Normalized):
 
 
 def LinearRegressionClassification(cluster, Normalized):
+    """
+        Does the linear regression from scratch
+    """
+    # Create an empty vector which will hold Accuracies of each experiment
     Accuracies = []
+
     for k in range(len(cluster.index)):
+        # Create a vector call subAccuracies which will append to Accuracies
         subAccuracies=[]
+
+        # This list will hold TPR values
         TPR_arr = []
+
+        # This list will hold FPR values
         FPR_arr = []
-        #f = open("experiment"+str(k+1)+".txt", "w")
+
+        # This will let you know what experiment you are on
         print("Starting Experiment "+str(k+1))
 
         # Gets the k row of the cluster data (33 random indices)
         test_data = cluster.iloc[k]
-        #print("Test data is:")
-        #print(test_data)
 
         # Gets everything else but the k row
         train_data = cluster.drop(cluster.index[k])
-        #print("Train Data is:")
-        #print(train_data)
 
         # Create data frame with actual data
         TEST_df = CreateDataFrame(test_data, Normalized)
+        # Print Test data labels to assure yourself, we are indeed changing test data each time
         print("Test data set labels are:")
+        # Values are located in first column of data
         print(TEST_df[0].values)
-        #f.write("Test dataframe being used is:\n")
-        #f.write(str(TEST_df.values)+"\n")
-        #print("Test dataframe with Normalized pixel values:")
-        #print(TEST_df)
 
-        # I was curious to see what 15x15 image looked like
-        #generateImages(TEST_df)
-
-        # Iterate through rows of
+        # Iterate through rows of the train data
         for i in range(len(train_data.index)):
-            #f.write("Training with \n")
 
             # Create dataframe of training data
             dummy = train_data.iloc[i]
             TRAIN_df = CreateDataFrame(dummy, Normalized)
-            #print("Train dataframe with normalized pixel values")
-            #print(TRAIN_df)
-            #f.write(str(TRAIN_df.values)+"\n")
 
             # FROM KANGS NOTES ... Aka magic
             n, p = TRAIN_df.shape # number of samples and features
 
-            # if y is greater than 5, class will be 1 otherwise 0
-            # we convert a regression problem to a classification problem (discretization)
             y = np.zeros(n)
             y[TRAIN_df[0] > 5] = 1
             # data preparation for training data
@@ -118,44 +111,42 @@ def LinearRegressionClassification(cluster, Normalized):
 
 
             b_opt = SolverLinearRegression(X, y)
-            #print("bopt is ")
-            #print(b_opt)
-
-            #print("np.dot(X_test, b_opt) is")
-            #print(np.dot(X_test, b_opt))
-
-            #print("np.array(np.dot(X_test, b_opt) > 0.5) is")
-            #print(np.array(np.dot(X_test, b_opt) > 0.5))
-
-            #print("sum(np.array(np.dot(X_test, b_opt) > 0.5) == y_groundtruth) is")
-            #print(sum(np.array(np.dot(X_test, b_opt) > 0.5) == y_groundtruth))
+            # Append TPR values to corresponding array
             TPR_arr.append(float(sum(np.array(np.dot(X_test, b_opt) > 0.5) == y_groundtruth))/33)
+            # Append FPR values to corresponding array
             FPR_arr.append( float(33 - sum(np.array(np.dot(X_test, b_opt) > 0.5) == y_groundtruth))/33)
-            SUMMER = sum(np.array(np.dot(X_test, b_opt) > 0.5) == y_groundtruth) / len(y_groundtruth)
-            subAccuracies.append(SUMMER)
-            #print("Accuracy is "+str(SUMMER))
-            #f.write("Accuracy is "+str(SUMMER)+"\n")
-            # visualization of the result
-#            plt.plot(np.dot(X_test, b_opt), 'ro', y, 'bo')
-            #plt.show()
-        # intialise data of lists.
+            # Append Accuracies to corresponding array
+            subAccuracies.append(sum(np.array(np.dot(X_test, b_opt) > 0.5) == y_groundtruth) / len(y_groundtruth))
+
+        # Make a dictionary of data
         data = {'TPR':TPR_arr, 'FPR':FPR_arr, 'Accuracies':subAccuracies}
+        # Create dataframe with the data
         TprFprFrame = pd.DataFrame(data)
+        # Print it
         print(TprFprFrame)
+        # Add a line feed for readability
         print("\n")
+        # Append to the bigger Accuracies vector
         Accuracies.append(subAccuracies)
-        #f.close()
-    print("Accuracies of each experiment are: ")
+
+    # Once done looping, crete a dataframe with Accuracies
     df = pd.DataFrame(Accuracies)
-    print(df)
+
+    # aoee stands for Accuracies Of Each Experiment (AOEE)
     aoee = []
+    # Loop through dataframe of Accuracies
     for i in range(len(df.index)):
-        a = statistics.mean(df.iloc[i].values)
+        # Get the Mean of each row... which holds Accuracies of each sub experiment
+        a = statistics.mean(df.iloc[i].values
+        # Append to Aoee
         aoee.append(a)
-    #print(aoee)
+    # Cteate another data dict
     data = {'Averaged Accuracies':aoee,'Experiment': [1, 2,3,4,5,6,7,8,9,10]}
+    # Create dataframe
     averages_of_experiments = pd.DataFrame(data)
+    # Set index to experiments... so it looks nicer when opened in excel or numbers
     averages_of_experiments = averages_of_experiments.set_index('Experiment')
+    # Show yourself what you gout.
     print("Averages of each experiment are:")
     print(averages_of_experiments)
 
@@ -178,7 +169,6 @@ def main():
     len_Normalized = len(Normalized.index)
     #print(len_Normalized) # TEST Print
 
-    threshold = 0.5
     k = 10
 
     members_in_cross_validation = int(len_Normalized/k)
@@ -193,7 +183,6 @@ def main():
     # Create Dataframe of randomized indices called df
     df = pd.DataFrame(cross_validation_vectors)
 
-    #print(df) # TEST PRINT
 
     # Let the classification  begin
     LinearRegressionClassification(df,Normalized)
